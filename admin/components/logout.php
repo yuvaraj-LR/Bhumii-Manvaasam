14145e689b6c5fece9a363240f2659bef66d8ca5
yuvaraj-LR <yuvarajrama121@gmail.com>
2023-03-25 12:37:54 +0530

    Bhumii Client Website Code

--- admin/components/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-include_once("../../constant.php");
-session_start();
-$json["data"] = [];
-session_destroy();
-$json["error"] = array("code" => "#200", "description" => "Success.");
-header("Location: /admin/");
